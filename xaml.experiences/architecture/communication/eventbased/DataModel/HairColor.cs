842f5f579338fa294d45cc379de75748bd753fe3
Håvard Moås <haavamoa@gmail.com>
2019-05-13 07:19:55 +0200

    Fixed references and generated GetHashCode for HairColor

--- xaml.experiences/architecture/communication/eventbased/DataModel/HairColor.cs
+++ xaml.experiences/architecture/communication/eventbased/DataModel/HairColor.cs
@@ -12,5 +12,15 @@ namespace eventbased.DataModel
         }
 
         public override bool Equals(object other) => other is HairColor && ((HairColor)other).ColorName.Equals(ColorName, StringComparison.OrdinalIgnoreCase);
+
+        protected bool Equals(HairColor other)
+        {
+            return string.Equals(ColorName, other.ColorName);
+        }
+
+        public override int GetHashCode()
+        {
+            return (ColorName != null ? ColorName.GetHashCode() : 0);
+        }
     }
 }
